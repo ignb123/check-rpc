fe97305e9a9eb48c228995ea44bc89d761d9f677
ignb123 <1403378882@qq.com>
2024-05-23 22:53:48 +0800

    version35:基于SPI扩展加权轮询算法负载均衡策略

--- checkrpc-test/checkrpc-test-provider/src/main/java/io/check/rpc/test/provider/single/RpcSingleServerTest.java
+++ checkrpc-test/checkrpc-test-provider/src/main/java/io/check/rpc/test/provider/single/RpcSingleServerTest.java
@@ -6,8 +6,9 @@ import org.junit.Test;
 public class RpcSingleServerTest {
     @Test
     public void startRpcSingleServer(){
-        RpcSingleServer singleServer = new RpcSingleServer("127.0.0.1:27880","127.0.0.1:2181",
-                "zookeeper","random", "io.check.rpc.test",
+        RpcSingleServer singleServer = new RpcSingleServer("127.0.0.1:27880",
+                "127.0.0.1:2181",
+                "zookeeper","robinweight", "io.check.rpc.test",
                 "javassist");
         singleServer.startNettyServer();
     }
