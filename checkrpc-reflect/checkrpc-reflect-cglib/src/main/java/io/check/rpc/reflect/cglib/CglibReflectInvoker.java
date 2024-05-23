e67097e7991ed058e97bf1916c661d098bc9de69
ignb123 <1403378882@qq.com>
2024-05-23 21:40:00 +0800

    version32:基于SPI扩展CGLib反射机制调用真实方法

---
+++ checkrpc-reflect/checkrpc-reflect-cglib/src/main/java/io/check/rpc/reflect/cglib/CglibReflectInvoker.java
@@ -0,0 +1,22 @@
+package io.check.rpc.reflect.cglib;
+
+import io.check.rpc.reflect.api.ReflectInvoker;
+import io.check.rpc.spi.annotation.SPIClass;
+import net.sf.cglib.reflect.FastClass;
+import net.sf.cglib.reflect.FastMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SPIClass
+public class CglibReflectInvoker implements ReflectInvoker {
+
+    private final Logger logger = LoggerFactory.getLogger(CglibReflectInvoker.class);
+
+    @Override
+    public Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName, Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
+        logger.info("use cglib reflect type invoke method...");
+        FastClass serviceFastClass  = FastClass.create(serviceClass);
+        FastMethod serviceFastMethod = serviceFastClass.getMethod(methodName, parameterTypes);
+        return serviceFastMethod.invoke(serviceBean, parameters);
+    }
+}
--- checkrpc-test/checkrpc-test-provider/src/main/java/io/check/rpc/test/provider/single/RpcSingleServerTest.java
+++ checkrpc-test/checkrpc-test-provider/src/main/java/io/check/rpc/test/provider/single/RpcSingleServerTest.java
@@ -7,7 +7,7 @@ public class RpcSingleServerTest {
     @Test
     public void startRpcSingleServer(){
         RpcSingleServer singleServer = new RpcSingleServer("127.0.0.1:27880","127.0.0.1:2181",
-                "zookeeper", "io.check.rpc.test","jdk");
+                "zookeeper", "io.check.rpc.test","cglib");
         singleServer.startNettyServer();
     }
 }
