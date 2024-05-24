e9214cca95a6ce5324763e33d8361f7eee9d494d
ignb123 <1403378882@qq.com>
2024-05-24 11:01:47 +0800

    version39:在原有加权源IP地址Hash算法负载均衡策略进行加强

--- checkrpc-loadbalancer/checkrpc-loadbalancer-sourceip-hash-weight/src/main/java/io/check/rpc/loadbalancer/sourceip/hash/weight/SourceIpHashWeightServiceLoadBalancer.java
+++ checkrpc-loadbalancer/checkrpc-loadbalancer-sourceip-hash-weight/src/main/java/io/check/rpc/loadbalancer/sourceip/hash/weight/SourceIpHashWeightServiceLoadBalancer.java
@@ -1,6 +1,8 @@
 package io.check.rpc.loadbalancer.sourceip.hash.weight;
 
 import io.check.rpc.loadbalancer.api.ServiceLoadBalancer;
+import io.check.rpc.loadbalancer.base.BaseEnhancedServiceLoadBalancer;
+import io.check.rpc.protocol.meta.ServiceMeta;
 import io.check.rpc.spi.annotation.SPIClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,25 +11,21 @@ import org.springframework.util.StringUtils;
 import java.util.List;
 
 @SPIClass
-public class SourceIpHashWeightServiceLoadBalancer<T> implements ServiceLoadBalancer<T> {
+public class SourceIpHashWeightServiceLoadBalancer extends BaseEnhancedServiceLoadBalancer {
 
     private final Logger logger = LoggerFactory.getLogger(SourceIpHashWeightServiceLoadBalancer.class);
     @Override
-    public T select(List<T> servers, int hashCode, String sourceIp) {
-        logger.info("基于源IP地址加权Hash的负载均衡策略...");
+    public ServiceMeta select(List<ServiceMeta> servers, int hashCode, String ip) {
+        logger.info("增强型基于源IP地址加权Hash的负载均衡策略...");
+        servers = this.getWeightServiceMetaList(servers);
         if (servers == null || servers.isEmpty()){
             return null;
         }
         //传入的IP地址为空，则默认返回第一个服务实例m
-        if (StringUtils.isEmpty(sourceIp)){
+        if (StringUtils.isEmpty(ip)){
             return servers.get(0);
         }
-
-        int count = Math.abs(hashCode) % servers.size();
-        if (count == 0){
-            count = servers.size();
-        }
-        int resultHashCode = Math.abs(sourceIp.hashCode() + hashCode);
-        return servers.get(resultHashCode % count);
+        int resultHashCode = Math.abs(ip.hashCode() + hashCode);
+        return servers.get(resultHashCode % servers.size());
     }
 }
